d638ed16671cca28306f5f39d9a4db7284b6af6f
ch1ckenw1ng <vargad4876@gmail.com>
2021-03-24 00:13:12 -0500

    added let ='s

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+let question1 = document.getElementsByClassName("q1");
+let question2 = document.getElementsByClassName("q2");
+let question3 = document.getElementsByClassName("q3");
+let question4 = document.getElementsByClassName("q4");
+let question5 = document.getElementsByClassName("q5");
+
